b15d5806cc5fa785763b7dd8467763d14bae909d
Peau Clement <clement.peau@epitech.eu>
2016-04-18 00:16:30 +0200

    Git init guys <3

---
+++ inc/42s.h
@@ -0,0 +1,16 @@
+/*
+** 42s.h for 42.sh in /home/peau_c/rendu/PSU/PSU_2015_42sh
+**
+** Made by Poc
+** Login   <peau_c@epitech.net>
+**
+** Started on  Mon Apr 18 00:15:01 2016 Poc
+** Last update Mon Apr 18 00:15:41 2016 Poc
+*/
+
+#ifndef _42s_H_
+# define _42s_H_
+
+
+
+#endif /* _42s_H_ */
---
+++ src/main.c
@@ -0,0 +1,16 @@
+/*
+** main.c for main in /home/peau_c/rendu/PSU/PSU_2015_42sh
+**
+** Made by Poc
+** Login   <peau_c@epitech.net>
+**
+** Started on  Mon Apr 18 00:15:50 2016 Poc
+** Last update Mon Apr 18 00:16:07 2016 Poc
+*/
+
+#include "42s.h"
+
+int	main(int ac, char **av)
+{
+  return (0);
+}
