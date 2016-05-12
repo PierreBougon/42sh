bd1a20ab1d260220f8e7cb7657e85ba728b1d6fd
sauvau_m <sauvau_m@epitech.eu>
2016-05-12 15:32:10 +0200

    move cursor technique ok

--- inc/42s.h
+++ inc/42s.h
@@ -5,11 +5,20 @@
 ** Login   <peau_c@epitech.net>
 **
 ** Started on  Mon Apr 18 00:15:01 2016 Poc
-** Last update Mon May  9 15:06:11 2016 marel_m
+** Last update Thu May 12 15:23:04 2016 Mathieu Sauvau
 */
 
 #ifndef _42s_H_
 # define _42s_H_
+
+# define cursorforward(x) printf("\033[%dC", (x))
+# define cursorbackward(x) printf("\033[%dD", (x))
+
+typedef struct	s_key_action
+{
+  char		*key;
+  void		(*fct)(int *);
+}		t_key_act;
 
 typedef struct  s_node
 {
--- src/main.c
+++ src/main.c
@@ -5,14 +5,16 @@
 ** Login   <marel_m@epitech.net>
 **
 ** Started on  Wed Apr 27 18:00:58 2016 marel_m
-** Last update Wed May 11 18:48:12 2016 Mathieu Sauvau
+** Last update Thu May 12 15:27:48 2016 Mathieu Sauvau
 */
 
-#include <ncurses/curses.h>
+#include <sys/ioctl.h>
+#include <termios.h>
 #include <ncurses.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
 #include "42s.h"
 
@@ -28,41 +30,101 @@ void	my_show_tab(char **str)
     }
 }
 
-
-#define cursorforward(x) printf("\033[%dC", (x))
-#define cursorbackward(x) printf("\033[%dD", (x))
-
-int	main(int ac, char **av, char **env)
+void		move_left(int *pos)
 {
-  char	*str;
-  t_sh	sh;
-  char	c[2];
+  //  printf("move left");
+  cursorbackward(1);
+  fflush(stdout);
+}
+
+void		move_right(int *pos)
+{
+  /* printf("move right\n"); */
+  cursorforward(*pos++);
+  fflush(stdout);
+}
+
+void		init_actions(t_key_act actions[4])
+{
+  actions[0].key = tigetstr("kcub1");
+  actions[0].fct = &move_left;
+  actions[1].key = tigetstr("kcuf1");
+  actions[1].fct = &move_right;
+}
+
+void            change_read_mode(int i, int time, int nb_char)
+{
+  static struct termios old;
+  static struct termios new;
+
+  if (i == 0)
+    {
+      ioctl(0, TCGETS, &old);
+      ioctl(0, TCGETS, &new);
+      new.c_lflag &= ~(ICANON);
+      new.c_lflag &= ~(ECHO);
+      new.c_cc[VMIN] = nb_char;
+      new.c_cc[VTIME] = time;
+      ioctl(0, TCSETS, &new);
+    }
+  if (i == 1)
+    ioctl(0, TCSETS, &old);
+}
+
+
+int		do_action(t_key_act actions[2], char *str)
+{
+  static int		cur_pos;
+  char		buff[10];
+  char		*new;
+  int		i;
+
+  i = -1;
+  memset(buff, 0, 10);
+  //  cursorforward(10);
+  read(0, buff, 10);
+  while (++i < 2)
+    {
+      if (strcmp(buff, actions[i].key) == 0)
+	{
+	  actions[i].fct(&cur_pos);
+	  return (1);
+	}
+    }
+  write(1, buff, strlen(buff));
+  i = -1;
+  while (buff[++i])
+    {
+      if (buff[i] == '\n')
+	return (3);
+    }
+  return (0);
+}
+
+int		main(int ac, char **av, char **env)
+{
+  char		*str;
+  t_sh		sh;
+  t_key_act	actions[2];
+  int		a;
 
   if (check_env(&sh, env))
     return (-1);
   setupterm(NULL, 0, NULL);
-  c[0] = 0;
-  str = malloc(1000);
-  str[0] = 0;
+  printf("%s\n", tigetstr("smkx"));
+  init_actions(actions);
+  change_read_mode(0, 100, 0);
+  str = NULL;
+  a = 3;
   while (42)
     {
-      write(1, "hey ->", 7);
-      memset(str, 0, 1000);
-      while ((c[0] = getchar()) != '\n')
-	{
-	  c[1] = 0;
-	  strcat(str, c);
-	}
-      /* if ((str = get_next_line(0)) == NULL) */
-      /* 	/\* || (str = epur(str)) == NULL) *\/ */
+      if (a == 3)
+	write(1, "hey ->", 7);
+      a = do_action(actions, str);
+      /* if ((str = get_next_line(0)) == NULL */
+      /* 	  || (str = epur(str)) == NULL) */
       /* 	return (-1); */
-      printf("bite %s\n", str);
-      printf("left %s\n", tigetstr("kcub1"));
-      //      printf("right %d %d %d\n", s[0], s[1], s[2]);
-      if (strcmp(str, tigetstr("kcub1")) == 0)
-      	cursorbackward(1);
-      if (strcmp(str, tigetstr("kcuf1")) == 0)
-      	cursorforward(1);
+      free(str);
     }
   return (0);
 }
