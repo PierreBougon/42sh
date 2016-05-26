cd4c45ad00760556e4db1cf70a5dc28d2a98ca9b
marel_m <maud.marel@epitech.eu>
2016-05-26 11:45:20 +0200

    rm dossier

--- src/exec/builtin_or_exec.c
+++ src/exec/builtin_or_exec.c
@@ -5,13 +5,29 @@
 ** Login   <marel_m@epitech.net>
 **
 ** Started on  Wed May 18 17:16:18 2016 marel_m
-** Last update Tue May 24 12:27:43 2016 marel_m
+** Last update Thu May 26 11:41:09 2016 marel_m
 */
 
 #include <sys/wait.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include "42s.h"
+
+int	action_redir(t_sh *sh)
+{
+  if (sh->exec->fd != 1 && (sh->exec->type == REDIR_RIGHT
+			    || sh->exec->type == DOUBLE_REDIR_RIGHT))
+    {
+      if (dup2(sh->exec->fd, 1) == -1)
+	return (1);
+    }
+  else if (sh->exec->fd != 1 && sh->exec->type == REDIR_LEFT)
+    {
+      if (dup2(sh->exec->fd, 0) == -1)
+	return (1);
+    }
+  return (0);
+}
 
 int	action(t_sh *sh)
 {
@@ -22,18 +38,8 @@ int	action(t_sh *sh)
     return (1);
   if (pid == 0)
     {
-      if (sh->exec->fd != 1 && (sh->exec->type == REDIR_RIGHT
-				|| sh->exec->type == DOUBLE_REDIR_RIGHT))
-	{
-	  if (dup2(sh->exec->fd, 1) == -1)
-	    return (1);
-	}
-      else if (sh->exec->fd != 1 && sh->exec->type == REDIR_LEFT)
-	{
-	  if (dup2(sh->exec->fd, 0) == -1)
-	    return (1);
-	}
-      if (execve(sh->exec->good_path, sh->exec->arg, sh->env->env) == -1)
+      if (action_redir(sh)
+	  || execve(sh->exec->good_path, sh->exec->arg, sh->env->env) == -1)
 	return (1);
     }
   else if (pid != 0 && sh->exec->fd == 1)
