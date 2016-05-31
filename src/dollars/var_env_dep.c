f3b835deb4c98e7854979236d5ba07eb2647a8bc
debrau_c <carl.debrauwere@epitech.eu>
2016-05-31 21:39:10 +0200

    I need dollars

---
+++ src/dollars/var_env_dep.c
@@ -0,0 +1,50 @@
+/*
+** var_env_dep.c for azd in /home/debrau_c/Documents/cours/PSU/save_var_env/srcs
+**
+** Made by debrau_c
+** Login   <debrau_c@epitech.net>
+**
+** Started on  Tue May 31 14:24:01 2016 debrau_c
+** Last update Tue May 31 21:16:10 2016 debrau_c
+*/
+
+int	var_env_copy_fin(char *new, char **str, int j, int m)
+{
+  while (str[0][j] != '\0' && str[0][j] != ' ')
+    j++;
+  while (str[0][j] != '\0')
+    {
+      new[m] = str[0][j];
+      ++m;
+      ++j;
+    }
+  new[m] = '\0';
+  return (0);
+}
+
+int	var_env_place_k(char *env)
+{
+  int	k;
+
+  k = 0;
+  while (env[k] != '\0' && env[k] != '=')
+    ++k;
+  if (env[k] == '=')
+    ++k;
+  else
+    return (-1);
+  return (k);
+}
+
+int	var_env_place_j(char *new, char *str, int index)
+{
+  int	j;
+
+  j = 0;
+  while (str[j] != '\0' && j < index)
+    {
+      new[j] = str[j];
+      j = j + 1;
+    }
+  return (j);
+}
---
+++ src/dollars/var_env_str.c
@@ -0,0 +1,46 @@
+/*
+** my_string.c for azd in /home/debrau_c/bin/new_repo/srcs
+**
+** Made by debrau_c
+** Login   <debrau_c@epitech.net>
+**
+** Started on  Fri Apr 29 21:06:16 2016 debrau_c
+** Last update Tue May 31 21:16:03 2016 debrau_c
+*/
+
+#include <stdlib.h>
+
+int	var_env_strlen(char *str)
+{
+  int	i;
+
+  i = 0;
+  while (str[i]) i++;
+  return (i);
+}
+
+int	var_env_strclen(char *str, char c)
+{
+  int	i;
+
+  i = 0;
+  while (str[i] && str[i] != c) i++;
+  return (i);
+}
+
+char	*var_env_strcdup(char *str, char c)
+{
+  int	i;
+  char	*new;
+
+  i = 0;
+  if (!(new = malloc(sizeof(char) * (var_env_strclen(str, c) + 1))))
+      return (NULL);
+  while (str[i] && str[i] != c)
+    {
+      new[i] = str[i];
+      i++;
+    }
+  new[i] = '\0';
+  return (new);
+}
