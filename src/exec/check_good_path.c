48853a651a0c608eba25e5e214939469d0747a4d
marel_m <maud.marel@epitech.eu>
2016-05-31 15:53:37 +0200

    modif goo_path

--- src/exec/check_good_path.c
+++ src/exec/check_good_path.c
@@ -5,7 +5,7 @@
 ** Login   <marel_m@epitech.net>
 **
 ** Started on  Wed May 18 17:33:30 2016 marel_m
-** Last update Tue May 31 15:05:24 2016 marel_m
+** Last update Tue May 31 15:51:09 2016 marel_m
 */
 
 #include <string.h>
@@ -34,6 +34,12 @@ int	wrong_command(t_sh *sh)
 int	check_wrong_path(t_sh *sh)
 {
   sh->exec->stop = 1;
+  if (strncmp(sh->exec->exec, "./", 2) == 0)
+    {
+      if ((sh->exec->exec
+	   = my_strdup_bt(sh->exec->exec, 2, strlen(sh->exec->exec))) == NULL)
+	return (1);
+    }
   if (sh->exec->good_path == '\0')
     {
       sh->exit = 1;
@@ -79,6 +85,8 @@ int	check_good_path_normal_case(t_sh *sh)
 int	check_good_path(t_sh *sh)
 {
   int	ret;
+  char	*path;
+  char	*pt_slash;
 
   if (sh->exec->exec[0] == '/')
     {
@@ -94,9 +102,18 @@ int	check_good_path(t_sh *sh)
       if ((sh->exec->good_path = strdup(sh->exec->exec)) == NULL)
 	return (1);
     }
-  else
-    if ((ret = check_good_path_normal_case(sh)) != 0)
+  else if ((ret = check_good_path_normal_case(sh)) != 0)
       return (ret);
+  else
+    {
+      if ((path = strdup(sh->exec->exec)) == NULL
+	  || (sh->exec->exec = malloc(sizeof(char)
+				      * (strlen(path) + 3))) == NULL
+	  || (pt_slash = strdup("./")) == NULL
+	  || (sh->exec->exec = strcat(pt_slash, path)) == NULL
+	  || (sh->exec->good_path = strdup(sh->exec->exec)) == NULL)
+	return (1);
+    }
   if (check_wrong_path(sh))
     return (-1);
   return (0);
