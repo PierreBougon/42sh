22d6193172494701661b1e3c81efcbce66bcb40d
marel_m <maud.marel@epitech.eu>
2016-06-01 19:29:43 +0200

    parsing <<

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -5,7 +5,7 @@
 ** Login   <marel_m@epitech.net>
 **
 ** Started on  Wed May 11 16:02:55 2016 marel_m
-** Last update Wed Jun  1 18:46:38 2016 marel_m
+** Last update Wed Jun  1 19:29:21 2016 marel_m
 */
 
 #include <stdio.h>
@@ -77,7 +77,6 @@ int		stock_elem(t_sh *sh, char *str, int st, int end)
     }
   if (pars_tree(elem, elem->arg))
     return (1);
-  print_tree(elem->node);
   return (0);
 }
 
