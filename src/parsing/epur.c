7696e6403c657615a464e4c6c11736d263beadc3
marel_m <maud.marel@epitech.eu>
2016-06-02 17:12:52 +0200

    gestion double quote mais pas norme

--- src/parsing/epur.c
+++ src/parsing/epur.c
@@ -5,7 +5,7 @@
 ** Login   <peau_c@epitech.net>
 **
 ** Started on  Mon May  9 13:23:09 2016
-** Last update Thu Jun  2 16:32:19 2016 marel_m
+** Last update Thu Jun  2 17:05:46 2016 marel_m
 */
 
 #include <stdlib.h>
@@ -50,14 +50,17 @@ char	*epur(char *str)
   while (str[i] != '\0' && str)
     {
       if (str[i] == '"')
-      	while (str[++i] != '"' && str[i] != '\0');
+	{
+	  new_str[j++] = '"';
+	  while (str[++i] != '"' && str[i] != '\0')
+	    new_str[j++] = str[i];
+	}
       else
       	{
 	  new_str = space_around_act(str, new_str, &i, &j);
 	  if (str[i] != '\0' && str)
-	    new_str[j++] = str[i];
+	    new_str[j++] = str[i++];
 	}
-      i++;
     }
   new_str[j] = 0;
   return (new_str);
