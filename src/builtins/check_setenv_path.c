5c361a5ffb676583307628f5f944839cc7d567d2
marel_m <maud.marel@epitech.eu>
2016-06-04 23:57:53 +0200

    setenv PATH ok

--- src/builtins/check_setenv_path.c
+++ src/builtins/check_setenv_path.c
@@ -5,7 +5,7 @@
 ** Login   <marel_m@epitech.net>
 **
 ** Started on  Fri Jun  3 00:05:25 2016 marel_m
-** Last update Fri Jun  3 00:07:52 2016 marel_m
+** Last update Sat Jun  4 23:57:08 2016 marel_m
 */
 
 #include <stdlib.h>
@@ -15,13 +15,12 @@ int	check_setenv_path(t_sh *sh)
 {
   free_tab(sh->env->path);
   free(sh->env->path);
-  if (sh->exec->arg[2] == NULL)
+  if (sh->exec->arg[2] == NULL || strlen(sh->exec->arg[2]) == 0)
     {
       if (check_path_setenv(sh))
 	return (1);
     }
-  else
-    if (check_path(sh))
-     return (1);
+  else if (check_path(sh))
+    return (1);
   return (0);
 }
