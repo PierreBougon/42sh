96c2f573cd84a0c19215b0fb21339e6d62ff76cd
marel_m <maud.marel@epitech.eu>
2016-06-05 15:05:54 +0200

    start norme

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -5,7 +5,7 @@
 ** Login   <marel_m@epitech.net>
 **
 ** Started on  Wed May 11 16:02:55 2016 marel_m
-** Last update Sun Jun  5 14:11:52 2016 marel_m
+** Last update Sun Jun  5 15:02:02 2016 marel_m
 */
 
 #include <string.h>
@@ -61,8 +61,7 @@ int		pars_tree(t_list_sh *elem, char *str)
   if (check_prior(str) != 0)
     if (pars_tree(elem, str))
       return (1);
-  my_free((void **)&str);
-  return (0);
+  return (my_free((void **)&str), 0);
 }
 
 int		stock_elem(t_sh *sh, char *str, int st, int end)
